25f38e4f3af0b588dbf68daa20d3285eba27c3a7
Jonathan Bayless <baylessj@purdue.edu>
2020-10-06 14:02:05 -0400

    latest keymap modifications + pull remote for gergoplex

--- keyboards/keebio/iris/keymaps/baylessj/keymap.c
+++ keyboards/keebio/iris/keymaps/baylessj/keymap.c
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     OSL(1),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     MO(1),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LCPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RAPC,
   //└────────┴────────┴────────┴────────┴────────┴────────┴                          ├────────┼────────┼────────┼────────┼────────┴────────┘
-                       TT(2),   KC_LSFT, KC_SPC,  KC_BSPC,                            KC_LGUI, KC_TAB,  KC_ENT,  KC_RALT
+                       MO(2),   KC_LSFT, KC_SPC,  KC_BSPC,                            KC_LGUI, KC_TAB,  KC_ENT,  KC_RALT
                    // └────────┴────────┴────────┘────────┘                          └────────┴────────┴────────┴────────┘
   ),
 
